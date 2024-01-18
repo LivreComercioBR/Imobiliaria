ff3c6fdac995404098da9233fb7b3f01e9608089
Ronaldo Correia <ronaldo312006@hotmail.com>
2024-01-17 23:08:25 -0300

    Projeto Inicial Configurado
    O  projeto está rodando com a model de usuario e o admin configurado. O próximo passo é o backend.

---
+++ imobi_app/admin.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
+
+# Register your models here.
+
+@admin.register(User)
+class PersonAdmin(UserAdmin):
+    list_display = ('username', 'email', 'is_active', 'is_staff',)
+    readonly_fields = ('password',)
+    search_fields = ('username', 'email')
+    list_filter = ('username',)
+
+    def alterar_dados(self, request, user):
+        user = User.objects.all()
---
+++ imobi_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ImobiAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'imobi_app'
